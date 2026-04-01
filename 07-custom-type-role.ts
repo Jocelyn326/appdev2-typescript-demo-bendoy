88b4c9fff6b12f37ac30b29e730995a19a0eaae4
Cie <jocelynbendoy26@gmail.com>
2026-04-01 18:05:28 +0800

    Done playing and updating the 07-custom-type-role.ts file

--- 07-custom-type-role.ts
+++ 07-custom-type-role.ts
@@ -1,21 +1,37 @@
 (() => {
     // Type Aliases or Custom Types
     // use `type` keyword to create custom type
-    type Role = 'admin' | 'guess' | 'teacher' | 'student'
-
+    type UserRole = 'superadmin' | 'admin' | 'moderator' | 'user' | 'guest';
+    
     // Complex Object Type Definition
     type User = {
         name: string;
         age: number;
-        role: Role; // you can use as well the Role type in Object type
+        role: UserRole; // you can use as well the Role type in Object type
         permission: string[];
     }
 
-    let userRole: Role = 'admin'
+    let userRole: UserRole = 'admin'
+    console.log('userRole:', userRole)
 
-    // ...
+    // @ts-expect-error
+    userRole = 'teacher' // Error: Type '"teacher"' is not assignable to type 'UserRole'.
+    // @ts-expect-error
+    userRole = 0 // Error: Type '0' is not assignable to type 'UserRole'.
 
-    function access(role: Role) {
-        // ...
+
+    function access(role: UserRole) {
+        console.log('access role:', role)
+        role = 'superadmin' // OK
+        role = 'admin' // OK
+        // @ts-expect-error
+        role = 0 // Error: Type '0' is not assignable to type 'UserRole'.
+        // @ts-expect-error
+        role = true // Error: Type 'true' is not assignable to type 'UserRole'.
+        // @ts-expect-error
+        role = 'teacher' // Error: Type '"teacher"' is not assignable to type 'UserRole'.
     }
-})
+
+    access('admin')
+    access('moderator')
+})()
